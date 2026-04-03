5cb99753995b1ca8e57f0edfb919539c7480ec1a
tetashler <tetashler@email.com>
2026-04-03 12:26:35 +0800

    add vercel analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import { Home } from './components/Home';
 import { LineScreen } from './components/LineScreen';
+import { Analytics } from '@vercel/analytics/react';
 
 function App() {
   const [selectedLine, setSelectedLine] = useState<string | null>(null);
@@ -12,6 +13,7 @@ function App() {
       ) : (
         <Home onSelectLine={setSelectedLine} />
       )}
+      <Analytics />
     </>
   );
 }
